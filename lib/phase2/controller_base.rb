0f34e6c46b79b0c1181d81ed8f6ccebc3ac7db5e
Mark Azemoon <mmazemoon@gmail.com>
2015-05-19 15:14:59 -0700

    Implement phase 2 of ControllerBase which takes HTTP request object and processes it to fill response object

--- lib/phase2/controller_base.rb
+++ lib/phase2/controller_base.rb
@@ -4,20 +4,30 @@ module Phase2
 
     # Setup the controller
     def initialize(req, res)
+      @req = req
+      @res = res
     end
 
     # Helper method to alias @already_built_response
     def already_built_response?
+      @already_built_response
     end
 
     # Set the response status code and header
     def redirect_to(url)
+      @res["location"]= url
+      @res.status = 302
+
     end
 
     # Populate the response with content.
     # Set the response's content type to the given type.
     # Raise an error if the developer tries to double render.
     def render_content(content, content_type)
+      @res.content_type= content_type
+      @res.body = content
+      @already_built_response = true
+      @res
     end
   end
 end
