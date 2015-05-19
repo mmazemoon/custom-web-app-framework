a891cdfec39ef4c90c45a9c9ecd2d59888e961d2
Mark Azemoon <mmazemoon@gmail.com>
2015-05-19 14:16:09 -0700

    Add trap command to explicitly call shutdown when we interrupt

--- bin/p01_basic_server.rb
+++ bin/p01_basic_server.rb
@@ -5,4 +5,10 @@ require 'webrick'
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/Cookie.html
 
-WEBrick::HTTPServer.new(Port: 3000)
+server = WEBrick::HTTPServer.new(Port: 3000)
+
+trap ('INT') do
+  server.shutdown
+end
+
+server.start
