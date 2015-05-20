485d3561ab533e85085083920973fd25c81ee0a3
Mark Azemoon <mmazemoon@gmail.com>
2015-05-20 11:48:11 -0700

    Parse params that come in from query string

--- lib/phase5/params.rb
+++ lib/phase5/params.rb
@@ -10,6 +10,8 @@ module Phase5
     # You haven't done routing yet; but assume route params will be
     # passed in as a hash to `Params.new` as below:
     def initialize(req, route_params = {})
+      @params = route_params
+      parse_www_encoded_form(req.query_string)
     end
 
     def [](key)
@@ -24,15 +26,23 @@ module Phase5
     private
     # this should return deeply nested hash
     # argument format
-    # user[address][street]=main&user[address][zip]=89436
+    # user[address][street] = main & user[address][zip] = 89436
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
+      query_array = decode_www_form(www_encode_form) # => returns an array of [k, v] pairs  [[‘a’, ‘1’], [‘a’, ‘2’], [‘b’, ‘3’]]
+      query_array.each do |pair|
+        key, val = pair[0], pair[1]
+        @params[key] = val
+      end
     end
 
     # this should return an array
     # user[address][street] should return ['user', 'address', 'street']
     def parse_key(key)
+
     end
   end
 end
+
+# Decode URL-encoded form data from given str.
