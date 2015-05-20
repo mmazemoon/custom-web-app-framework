aeda9ebf77e8a92556bab49286b203352cf25053
Mark Azemoon <mmazemoon@gmail.com>
2015-05-20 15:02:21 -0700

    Implement behavior to check if route pattern and method match request path and method.

--- lib/phase6/router.rb
+++ lib/phase6/router.rb
@@ -3,15 +3,23 @@ module Phase6
     attr_reader :pattern, :http_method, :controller_class, :action_name
 
     def initialize(pattern, http_method, controller_class, action_name)
+      @pattern = pattern
+      @http_method = http_method
+      @controller_class = controller_class
+      @action_name = action_name
     end
 
     # checks if pattern matches path and method matches request method
     def matches?(req)
+      byebug
+      @http_method == req.request_method.downcase.to_sym &&
+      @pattern =~ req.path
     end
 
     # use pattern to pull out route params (save for later?)
     # instantiate controller and call controller action
     def run(req, res)
+
     end
   end
 
