eba7ce53fd2c2c7bfc5c9cc8e9528a5c933bfaf2
Mark Azemoon <mmazemoon@gmail.com>
2015-05-20 15:54:07 -0700

    Implement add_route method which constructs route and adds it to the router's list.

--- lib/phase6/router.rb
+++ lib/phase6/router.rb
@@ -27,20 +27,31 @@ module Phase6
     attr_reader :routes
 
     def initialize
+      @routes = []
     end
 
     # simply adds a new route to the list of routes
     def add_route(pattern, method, controller_class, action_name)
+      @routes << Route.new
     end
 
     # evaluate the proc in the context of the instance
     # for syntactic sugar :)
     def draw(&proc)
+
+
     end
 
     # make each of these methods that
     # when called add route
     [:get, :post, :put, :delete].each do |http_method|
+      define_method(http_method) do |pattern, controller_class, action_name |
+        add_route(pattern, http_method, controller_class, action_name)
+      end
+    end
+
+    def get(pattern, controller_class, action_name)
+      add_route(pattern, "get", controller_class, action_name)
     end
 
     # should return the route that matches this request
